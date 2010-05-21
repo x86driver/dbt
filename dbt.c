7672983a4138237674d68e25dcf2ddf24e830fab
Doremi Lin <doremi@doremi-desktop.garmin.com.tw>
2010-05-21 15:37:49 +0800

    尚未完成 只是先弄個雛型

--- dbt.c
+++ dbt.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
+#include "intermediate.h"
+
+struct cpu_state {
+	unsigned char codegen[512];
+};
+
+void showa(void)
+{
+	printf("Hello A\n");
+}
+
+/* helper function prototype (no argument, no return)
+ * void func(void);
+ */
+
+void gen_op_call0(struct cpu_state cpu, void *(func)(void))
+{
+}
+
+void translate()
+{
+}
 
 int main()
 {
